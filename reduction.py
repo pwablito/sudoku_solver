f2e412ba3abce9dc35af6846f814ec70a3def4c2
Spencer, Paul Whiting <pwablito@gmail.com>
2022-01-04 17:54:01 -0500

    block reduction algorithm

--- reduction.py
+++ reduction.py
@@ -1,3 +1,6 @@
+from puzzle import EMPTY
+
+
 class ReductionStrategy:
     def __init__(self, puzzle):
         self.puzzle = puzzle
@@ -11,7 +14,24 @@ class ReductionStrategy:
 
 class BlockReductionStrategy(ReductionStrategy):
     def reduce(self):
-        raise NotImplementedError
+        updated = False
+        for row_offset in [0, 3, 6]:
+            for col_offset in [0, 3, 6]:
+                available_values = list(range(1, 10))
+                for row in range(row_offset, row_offset + 3):
+                    for col in range(col_offset, col_offset + 3):
+                        item = self.puzzle.arr[row][col]
+                        if item != EMPTY:
+                            assert item in available_values
+                            available_values.remove(item)
+                if len(available_values) == 1:
+                    for row in range(row_offset, row_offset + 3):
+                        for col in range(col_offset, col_offset + 3):
+                            item = self.puzzle.arr[row][col]
+                            if item == EMPTY:
+                                self.puzzle.arr[row][col] = available_values[0]
+                                updated = True
+        return self.puzzle, updated
 
 
 class RowReductionStrategy(ReductionStrategy):
