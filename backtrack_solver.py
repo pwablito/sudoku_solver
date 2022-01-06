73fc22f3c7c2a10129f0239c3e2e2b2b428a50de
Spencer, Paul Whiting <pwablito@gmail.com>
2022-01-06 00:40:26 -0500

    Implement backtrack

--- backtrack_solver.py
+++ backtrack_solver.py
@@ -1,6 +1,34 @@
+from puzzle import EMPTY, Puzzle
 from solver import PuzzleSolver
 
 
 class BacktrackPuzzleSolver(PuzzleSolver):
+    @staticmethod
+    def first_empty_coords(arr):
+        for i in range(9):
+            for j in range(9):
+                if arr[i][j] == EMPTY:
+                    return i, j
+        raise ValueError("No empty cells")
+
     def solve(self):
-        raise NotImplementedError
+        state = self.puzzle.arr.copy()
+        solution = BacktrackPuzzleSolver.rec_solve(state)
+        self.puzzle = Puzzle(solution)
+
+    @staticmethod
+    def rec_solve(state):
+        '''
+        @return 2D array or False
+        '''
+        i, j = BacktrackPuzzleSolver.first_empty_coords(state)
+        for test_val in range(1, 10):  # Numbes 1-9
+            state[i][j] = test_val
+            if Puzzle(state).is_valid():
+                if Puzzle(state).is_solved():
+                    return state
+                solution_or_false = BacktrackPuzzleSolver.rec_solve(state)
+                if solution_or_false:
+                    return solution_or_false
+        state[i][j] = EMPTY
+        return False
